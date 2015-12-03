b9053480b14366acc98cdfee706471859ec60fc7
He Wenxiang <hewx@doordu.com>
2015-12-03 22:24:30 +0800

    init blog

---
+++ pelicanconf.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'erhuabushuo<at>gmail.com'
+SITENAME = '疯人院主任'
+SITEURL = ''
+
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Shanghai'
+
+DEFAULT_LANG = 'cn'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('伴生活', 'http://banshenghuo.com/'),
+         ('多度科技', 'http://doordu.com/'),)
+
+# Social widget
+SOCIAL = (('E-mail', 'mailto:erhuabushuo(at)gmail.com'),
+          ('github', 'http://github.com/erhuabushuo'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
